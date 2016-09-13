d839876c8bb12f2a1aeb70bbbaf4ddeb4ff2e56a
Trevor Jones <trevordavidjones@gmail.com>
2016-09-13 19:49:58 -0400

    refactored my gem

--- lib/nyc_green_markets.rb
+++ lib/nyc_green_markets.rb
@@ -15,21 +15,14 @@ module NycGreenMarkets
     def self.all
       markets_array = Unirest.get('https://data.cityofnewyork.us/resource/cw3p-q2v6.json').body
       markets = []
-      markets_array.each do |market|
-        markets << GreenMarket.new(market)
-      end
-      markets
+      create_markets(markets_array)
     end
 
     def self.where(search_term)
       key = search_term.keys.first.to_s
       value = search_term.values.first
       markets_array = Unirest.get("https://data.cityofnewyork.us/resource/cw3p-q2v6.json?#{key}=#{value}").body
-      markets = []
-      markets_array.each do |market|
-        markets << GreenMarket.new(market)
-      end
-      markets
+      create_markets(markets_array)
     end
 
     def self.find_by(search_term)
@@ -37,6 +30,12 @@ module NycGreenMarkets
       value = search_term.values.first
       market = Unirest.get("https://data.cityofnewyork.us/resource/cw3p-q2v6.json?#{key}=#{value}").body.first
       GreenMarket.new(market)
+    end
+
+    private_class_method :create_markets
+
+    def self.create_markets(markets_array)
+      markets_array.map { |market| GreenMarket.new(market)}
     end
   end
 end
