f1146e04bb64a5a835347b8dad7c61f02cbece3c
Trevor Jones <trevordavidjones@gmail.com>
2016-09-13 19:19:40 -0400

    add all class method

--- lib/nyc_green_markets.rb
+++ lib/nyc_green_markets.rb
@@ -1,5 +1,24 @@
 require "nyc_green_markets/version"
+require 'unirest'
 
 module NycGreenMarkets
-  # Your code goes here...
+  class GreenMarket
+    attr_reader :name, :address, :city, :zip
+
+    def initialize(market)
+      @name = market['facilityname']
+      @address = market['facilitystreetname']
+      @city = market['facilitycity']
+      @zip = market['facilityzipcode']
+    end
+
+    def self.all
+      markets_array = Unirest.get('https://data.cityofnewyork.us/resource/cw3p-q2v6.json').body
+      markets = []
+      markets_array.each do |market|
+        markets << GreenMarket.new(market)
+      end
+      markets
+    end
+  end
 end
