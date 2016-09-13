a7cd4afa0532626ce9a22389e68c5033c9258d51
Trevor Jones <trevordavidjones@gmail.com>
2016-09-13 19:38:13 -0400

    add where class method

--- lib/nyc_green_markets.rb
+++ lib/nyc_green_markets.rb
@@ -21,6 +21,17 @@ module NycGreenMarkets
       markets
     end
 
+    def self.where(search_term)
+      key = search_term.keys.first.to_s
+      value = search_term.values.first
+      markets_array = Unirest.get("https://data.cityofnewyork.us/resource/cw3p-q2v6.json?#{key}=#{value}").body
+      markets = []
+      markets_array.each do |market|
+        markets << GreenMarket.new(market)
+      end
+      markets
+    end
+
     def self.find_by(search_term)
       key = search_term.keys.first.to_s
       value = search_term.values.first
